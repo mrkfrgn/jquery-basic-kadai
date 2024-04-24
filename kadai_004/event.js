5303168afa30321a4e44bff7136d8bed5e766b42
古堅　まり子 <159230391+mrkfrgn@users.noreply.github.com>
2024-04-24 18:20:37 +0900

    Add files via upload

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,11 +1,8 @@
-$(function() {
-  $('window').on('load', function(){
+
+  $(window).on('load', function(){
     console.log('loadイベントが発生しました');
   });
-});
 
-$(function() {
-  $('window').on('scroll', function(){
+  $(window).on('scroll', function(){
     console.log('scrollイベントが発生しました');
   });
-});
